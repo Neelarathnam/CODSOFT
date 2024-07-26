6757d4ab2c289c4914b88e14ff6407a2bdef25c4
Neelarathnam <152837184+Neelarathnam@users.noreply.github.com>
2024-07-26 21:50:57 +0530

    Create Calculator.cpp

---
+++ Calculator.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"-------Welcome User-------"<<endl;
+double a,b;
+cout<<"Enter the first number: "<<endl;
+cin>>a;
+cout<<"Enter the the second number: "<<endl;
+cin>>b;
+char c;
+cout<<"Enter the arithmetic operator: "<<endl;
+cin>>c;
+double d;
+ switch(c)
+{
+case '+':
+d=a+b;
+break;
+case '-':
+d=a-b;
+break;
+case '*':
+d=a*b;
+break;
+case '/':
+d=a/b;
+break;
+default:
+cout<<"Invalid Operation";
+break;
+}
+cout<<"The value is: "<<d<<endl;
+cout<<"Welcome";
+}
